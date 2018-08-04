6d0fbd53f79e0f48add4f0a5a4f328dbfc1a89d7
kwzm <kwzm93@163.com>
2018-08-04 17:31:54 +0800

    add css less loader

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 import a from './a'
+import '../styles/index.css'
+import '../styles/reset.less'
+import '../styles/layer.less'
 
 let str = 'hello webpack'
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,34 @@ module.exports = {
   module: {
     rules: [
       {
-        test: '\.js$',
-        use: ['babel-loader']
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          }
+        ],
+        exclude: path.resolve(__dirname, 'node_modules'),
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          "postcss-loader",
+          "less-loader"
+        ]
       }
     ]
   },
